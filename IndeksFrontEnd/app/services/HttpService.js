42a31cd470464732cc5f538524c2f9984db6cb4b
dejanjanjic <dejan.janjic@student.etf.unibl.org>
2024-12-10 18:35:18 +0100

    access to protected apis

--- IndeksFrontEnd/app/services/HttpService.js
+++ IndeksFrontEnd/app/services/HttpService.js
@@ -1,7 +1,21 @@
 import { API_URL } from "@env";
+import authStorage from "../auth/storage";
 
 class HttpService {
   baseUrl = API_URL;
+
+  async getHeaders() {
+    const token = await authStorage.getToken();
+    const headers = {
+      "Content-Type": "application/json",
+    };
+
+    if (token) {
+      headers["Authorization"] = `Bearer ${token}`;
+    }
+
+    return headers;
+  }
 
   async handleResponse(response) {
     if (!response.ok) {
@@ -19,11 +33,10 @@ class HttpService {
 
   async create(resource, data) {
     console.log(`${this.baseUrl}/${resource}`);
+    const headers = await this.getHeaders();
     const response = await fetch(`${this.baseUrl}/${resource}`, {
       method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
+      headers,
       body: JSON.stringify(data),
     });
     console.log("Status Code:", response.status);
@@ -32,43 +45,39 @@ class HttpService {
 
   async get(resource) {
     console.log(`${this.baseUrl}/${resource}`);
+    const headers = await this.getHeaders();
     const response = await fetch(`${this.baseUrl}/${resource}`, {
       method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
+      headers,
     });
     return this.handleResponse(response);
   }
 
   async getById(resource, id = "") {
     console.log(`${this.baseUrl}/${resource}`);
+    const headers = await this.getHeaders();
     const response = await fetch(`${this.baseUrl}/${resource}/${id}`, {
       method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
+      headers,
     });
     return this.handleResponse(response);
   }
 
   async update(resource, id, data) {
+    const headers = await this.getHeaders();
     const response = await fetch(`${this.baseUrl}/${resource}/${id}`, {
       method: "PUT",
-      headers: {
-        "Content-Type": "application/json",
-      },
+      headers,
       body: JSON.stringify(data),
     });
     return this.handleResponse(response);
   }
 
   async delete(resource, id) {
+    const headers = await this.getHeaders();
     const response = await fetch(`${this.baseUrl}/${resource}/${id}`, {
       method: "DELETE",
-      headers: {
-        "Content-Type": "application/json",
-      },
+      headers,
     });
     return this.handleResponse(response);
   }
